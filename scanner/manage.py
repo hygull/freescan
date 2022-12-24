8af95a157db1e4f59002d448c0a764fc860f3f5f
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2022-12-24 14:56:17 +0530

    applied the migration & updated manage.py (to reset DJANGO_SETTINGS_MODULE)

--- scanner/manage.py
+++ scanner/manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    os.environ.pop('DJANGO_SETTINGS_MODULE')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scanner.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +17,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    print('Command Line Arguments Received ->', sys.argv)
     execute_from_command_line(sys.argv)
 
 
