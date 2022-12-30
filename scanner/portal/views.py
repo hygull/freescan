3e7ed0b336134d299001c46e2f92eef182a7c4f1
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2022-12-30 15:40:17 +0530

    updated API to properly generate the PDF files (bug fix)

--- scanner/portal/views.py
+++ scanner/portal/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from django.views.generic.base import View
 from portal.models import Category
 import PyPDF2
+from io import BytesIO
+from reportlab.pdfgen import canvas
 from django.template.context_processors import csrf
 from rest_framework.parsers import FormParser, MultiPartParser, FileUploadParser, JSONParser
 import pandas as pd
@@ -38,7 +40,7 @@ class DocumentView(APIView):
         find_text = data.get('find_text', '')
         notes = data.get('notes', '')
         file =  data.get('file') # None
-
+        print('category', 'find_text', find_text)
         # ----
         # file = open('docs/sample.pdf', 'rb')
         pdf_reader = PyPDF2.PdfReader(file)
@@ -52,26 +54,56 @@ class DocumentView(APIView):
         print(df_final)
         print(Path.home())
 
+        # try:
+        #     path = os.sep.join(['docs', 'output', f'Scanned_Output_{datetime.now()}.pdf'])
+        #     with Path(path).open(mode='wb') as fw:
+        #         final_lines = '\n'.join(df_final['text'])
+        #         print('Ok->', final_lines, type(final_lines))
+        #         fw.write('Property Scan Findings\n'.encode('utf8'))
+        #         fw.write(final_lines.encode('utf8'))
+        #         fw.write(f'\n{notes}'.encode('utf8'))
+
+        #     content_type = 'application/pdf'
+        #     response = HttpResponse(open(path, 'rb'), content_type=content_type)
+        #     response['Content-Disposition'] = 'attachment; filename="Pdf Report - {}"'.format(path.split('/')[-1])
+        #     # ----
+
+        #     print('category', category, 'find_text', find_text, 'notes', notes, 'file', file)
+        #     # c = {}
+        #     # c.update(csrf(request))
+        #     return response
+        # except Exception as exc:
+        #     return Response({
+        #         'status': 400,
+        #         'message': f'{exc}'
+        #     })
+
         try:
-            path = os.sep.join(['docs', 'output', 'Scanned_Output_{datetime.now()}.pdf'])
-            with Path(path).open(mode='wb') as fw:
-                final_lines = '\n'.join(df_final['text'])
-                print('Ok->', final_lines, type(final_lines))
-                fw.write('Property Scan Findings\n')
-                fw.write(final_lines.encode('utf8'))
-                fw.write('\n' + notes)
+            print('Starting the work!')
+            op_buf = BytesIO()
+            writer = PyPDF2.PdfWriter()
+            print('canvas', canvas)
+            page = writer.add_blank_page(width=595, height=842)
+            canvas_obj = canvas.Canvas(op_buf, pagesize=(595, 842))
+            canvas_obj.drawString(100, 750, '\n'.join(df_final['text']))
+            canvas_obj.save()
+
+            filepath = os.sep.join(['docs', 'output', f'Scanned_Output_{datetime.now()}.pdf'])
+
+            with open(filepath, 'wb') as f:
+                f.write(op_buf.getvalue())
 
             content_type = 'application/pdf'
-            response = HttpResponse(open(path, 'rb'), content_type=content_type)
-            response['Content-Disposition'] = 'attachment; filename="Pdf Report - {}"'.format(path.split('/')[-1])
-            # ----
-
+            response = HttpResponse(open(filepath, 'rb'), content_type=content_type)
+            response['Content-Disposition'] = 'attachment; filename="Pdf Report - {}"'.format(filepath.split('/')[-1])
+        
+            print(filepath)
             print('category', category, 'find_text', find_text, 'notes', notes, 'file', file)
-            # c = {}
-            # c.update(csrf(request))
             return response
         except Exception as exc:
+            raise
             return Response({
                 'status': 400,
                 'message': f'{exc}'
             })
+
